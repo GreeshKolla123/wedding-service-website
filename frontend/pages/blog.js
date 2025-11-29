00b8d5bf2fdceff7f5cdc9de2971b63299a57820
GreeshKolla123 <2100030262@kluniversity.in>
2025-11-29 12:11:28 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/pages/blog.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+function BlogPage() {
+  return (
+    <div>
+      <Head>
+        <title>Blog Page</title>
+      </Head>
+      <h2>Blog</h2>
+      <p>This is the blog page.</p>
+    </div>
+  );
+}
+
+export default BlogPage;
